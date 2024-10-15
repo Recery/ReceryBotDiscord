721fb7637992b51c0ad90debfca3fa431b42871b
Recery <142628253+Recery@users.noreply.github.com>
2024-10-15 20:43:20 -0300

    Update available_commands.js

--- Scripts/available_commands.js
+++ Scripts/available_commands.js
@@ -1,3 +1,14 @@
-var availables_commands = [
-    require("Commands/Saludar.js")
-];
+const fs = require("fs");
+const path = require("path");   
+
+const commands_path = path.join(__dirname, "Commands");
+const command_files = fs.readdirSync(commands_path).filter(file => file.endsWith(".js"));
+
+const availables_commands = [];
+
+for (const file of command_files) {
+    const command = require(`./Commands/${file}`); // Importa el archivo
+    availables_commands.push(command); // Añádelo al array
+}
+
+module.exports = availables_commands;
