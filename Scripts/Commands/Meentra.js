ad9dc0c5d02d41774e8b2d82c44195981d97ee24
Recery <142628253+Recery@users.noreply.github.com>
2024-10-16 18:10:13 -0300

    Update Meentra.js

--- Scripts/Commands/Meentra.js
+++ Scripts/Commands/Meentra.js
@@ -4,7 +4,7 @@ class Meentra extends Command
 {
     execution(msg)
     {
-        if (Math.floor(Math.random() * 1) + 2  === 1)
+        if (Math.floor(Math.random() * 2) + 1  === 1)
             msg.reply(`${this.get_mention(msg)}, es grande... No te entra`);
         else
             msg.reply(`${this.get_mention(msg)}, es muy chiquito, seguro te entra`);
