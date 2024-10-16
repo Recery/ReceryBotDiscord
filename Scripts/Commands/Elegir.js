ed03262e9091f9e6fb3b44da31295997a642538d
Recery <142628253+Recery@users.noreply.github.com>
2024-10-15 23:10:57 -0300

    Update Elegir.js

--- Scripts/Commands/Elegir.js
+++ Scripts/Commands/Elegir.js
@@ -4,9 +4,11 @@ class Elegir extends Command
 {
     execution(msg)
     {
-        var opciones = msg.content.split(",");
+        var content = msg.content.replace("!elegir", "")
+        var opciones = content.split(",");
         var elegido = opciones[Math.floor(Math.random() * opciones.length)];
-        msg.reply("Elegí " + elegido + " por vos. La próxima no seas pajero y elegí una opción por tu cuenta.");
+
+        msg.reply("Elegí " + elegido.trim() + " por vos. La próxima no seas pajero y elegí una opción por tu cuenta.");
     }
 }
 
