bdec941a24862a3aee468c786d957cab103d1c0e
Recery <142628253+Recery@users.noreply.github.com>
2024-10-16 19:30:17 -0300

    girasol command

---
+++ Scripts/Commands/Girasol.js
@@ -0,0 +1,15 @@
+const Command = require("./command_cls.js")
+
+class Girasol extends Command
+{
+    execution(msg)
+    {
+        msg.reply(
+        {
+            content: "Plantaste un productor solar... Imagino que al frente, como corresponde",
+            files: [this.get_gifs_directory() + "/Girasol.gif"]
+        });
+    }
+}
+
+module.exports = new Girasol("!girasol");
