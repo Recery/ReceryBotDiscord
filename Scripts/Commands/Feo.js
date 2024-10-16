881bc932b527c7e6c9a3ee6b5ac2a41fc35a661b
Recery <142628253+Recery@users.noreply.github.com>
2024-10-16 17:07:07 -0300

    Create Feo.js

---
+++ Scripts/Commands/Feo.js
@@ -0,0 +1,11 @@
+const Command = require("./command_cls.js")
+
+class Feo extends Command
+{
+    execution(msg)
+    {
+        msg.reply(`${this.get_mention(msg)}?, sos un ${Math.floor(Math.random() * 100) + 1} % feo`)
+    }
+}
+
+module.exports = new Feo("!feo");
