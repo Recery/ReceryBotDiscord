8f67db533f4d98592369baa61dbcf57fa73c04f8
Recery <142628253+Recery@users.noreply.github.com>
2024-10-16 01:44:20 -0300

    Update SenkoBot.js

--- Scripts/Commands/SenkoBot.js
+++ Scripts/Commands/SenkoBot.js
@@ -4,7 +4,7 @@ class SenkoInfo extends Command
 {
     execution(msg)
     {
-        msg.reply(`Hola ${get_mention(msg)}, soy Senko Bot, un bot creado por SenkoSan12543. Soy argentino y me gustan los peces. Tengo una obsesión por el Terraria.`)
+        msg.reply(`Hola ${this.get_mention(msg)}, soy Senko Bot, un bot creado por SenkoSan12543. Soy argentino y me gustan los peces. Tengo una obsesión por el Terraria.`)
     }
 }
 
