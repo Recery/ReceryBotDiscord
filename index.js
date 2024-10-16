a5f08a5279249053e3443e50bb824527fa4550b7
Recery <142628253+Recery@users.noreply.github.com>
2024-10-16 13:29:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
+var sleep = false
 client.on(Events.MessageCreate, (msg) => {
   if (msg.content === "!senkohelp")
   {
@@ -24,6 +25,30 @@ client.on(Events.MessageCreate, (msg) => {
     }
     message = message.slice(0, -2);
     msg.reply(message);
+  }
+  else if (msg.content === !senkoawake)
+  {
+    if (sleep)
+    {
+      msg.reply("La puta madre para que me despertas... Terrible siesta me estaba haciendo");
+      sleep = false
+    }
+    else
+    {
+      msg.reply("Ya estoy despierto, no ves que tengo los ojos abiertos?")
+    }
+  }
+  else if (msg.content === !senkosleep)
+  {
+    if (sleep)
+    {
+      msg.reply("Zzz... (Ya estoy dormido pelotudo)");
+    }
+    else
+    {
+      msg.reply("Vamos carajo, ya me estaba cansando de vos... A dormir... Zzz")
+      sleep = true
+    }
   }
   else
   {
