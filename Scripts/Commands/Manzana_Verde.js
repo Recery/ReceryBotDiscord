cea90fb77059c7ded567379da6fcfdddec7502b5
Recery <142628253+Recery@users.noreply.github.com>
2024-10-18 17:21:12 -0300

    Update Manzana_Verde.js

--- Scripts/Commands/Manzana_Verde.js
+++ Scripts/Commands/Manzana_Verde.js
@@ -2,9 +2,27 @@ const Command = require("./command_cls.js")
 
 class ManzanaVerde extends Command
 {
+    users = [];
+
     execution(msg)
     {
-        msg.reply(`LA PUTA MADRE ${this.get_mention(msg)}, MANZANA VERDE <:ManzanaVerde:1296171434246410380>, MI COMIDA FAVORITA`)
+        var user_added = false;
+        var total_apples = 0;
+        for (const user in users)
+        {
+            if (user === this.users.user)
+            {
+                user_added = true
+                user.green_apples += 1;
+                total_apples = user.green_apples;
+                break;
+            }
+        }
+
+        if (!user_added)
+            this.users.push({user: this.get_mention(msg), green_apples: 1})
+
+        msg.reply(`${this.get_mention(msg)}, ahora tenes ${total_apples}<:ManzanaVerde:1296171434246410380>`)
     }
 }
 
