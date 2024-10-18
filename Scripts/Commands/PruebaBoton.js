68361adcfebba2b0e3d71babf4d33b93a06f1bb7
Recery <142628253+Recery@users.noreply.github.com>
2024-10-18 19:47:44 -0300

    Update PruebaBoton.js

--- Scripts/Commands/PruebaBoton.js
+++ Scripts/Commands/PruebaBoton.js
@@ -1,20 +1,21 @@
 const Command = require("./command_cls.js")
-const { ButtonBuilder, ButtonStyle } = require('discord.js');
+const { ButtonBuilder, ActionRowBuilder } = require('discord.js');
 
 class PruebaBoton extends Command
 {
     execution(msg)
     {
-        const button = new ButtonBuilder()
-            .setCustomId('boton_click')
-            .setLabel('Apretar boton')
-            .setStyle(1)
-            .setType(1);
+        const newButtonZones = new ActionRowBuilder().addComponents(
+            new ButtonBuilder()
+                .setCustomId('boton_click')
+                .setLabel('Apretar boton')
+                .setStyle(1)
+        );
 
         msg.reply(
         {
             content: "Este es un boton de prueba",
-            components: [button]
+            components: [newButtonZones]
         });
     }
 }
