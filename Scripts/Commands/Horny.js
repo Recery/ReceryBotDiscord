9f9f0503285cd74cf4d0953ce6ee27910140544d
Recery <142628253+Recery@users.noreply.github.com>
2024-10-21 12:50:20 -0300

    Update Horny.js

--- Scripts/Commands/Horny.js
+++ Scripts/Commands/Horny.js
@@ -4,7 +4,7 @@ class Horny extends Command
 {
     execution(msg)
     {
-        if (this.get_content() !== "")
+        if (this.get_content(msg) !== "")
             msg.reply(`${this.get_content(msg)} está un ${Math.floor(Math.random() * 100) + 1}% horny :hot_face:`);
         else
             msg.reply(`${this.get_mention(msg)} está un ${Math.floor(Math.random() * 100) + 1}% horny :hot_face:`);
