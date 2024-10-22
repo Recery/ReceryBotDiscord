b16760cbd4689a5bc90e01bcbc8c515f0d252ef2
Recery <142628253+Recery@users.noreply.github.com>
2024-10-21 23:48:40 -0300

    Create ContadorComandos.js

---
+++ Scripts/Commands/ContadorComandos.js
@@ -0,0 +1,12 @@
+const Command = require("./command_cls.js")
+
+class ContadorComandos extends Command
+{
+    async execution(msg)
+    {
+        let commands_used = await this.get_commands_used(msg);
+        msg.reply(`${this.get_mention(msg)}, usaste un total de ${commands_used}`);
+    }
+}
+
+module.exports = new ContadorComandos("!contadorcomandos");
