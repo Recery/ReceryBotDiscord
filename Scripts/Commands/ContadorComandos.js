e757f37ec96c5da3a97c083631bc90a45376ed6e
Recery <142628253+Recery@users.noreply.github.com>
2024-10-21 23:52:10 -0300

    Update ContadorComandos.js

--- Scripts/Commands/ContadorComandos.js
+++ Scripts/Commands/ContadorComandos.js
@@ -5,7 +5,7 @@ class ContadorComandos extends Command
     async execution(msg)
     {
         let commands_used = await this.get_commands_used(msg);
-        msg.reply(`${this.get_mention(msg)}, usaste un total de ${commands_used}`);
+        msg.reply(`${this.get_mention(msg)}, usaste un total de ${commands_used} comandos.`);
     }
 }
 
