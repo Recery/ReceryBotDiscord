2bf98b7dbfced6964bf6a7cd99db70f8cf33b270
Recery <142628253+Recery@users.noreply.github.com>
2024-10-21 21:35:55 -0300

    Update Cosa.js

--- Scripts/Commands/Cosa.js
+++ Scripts/Commands/Cosa.js
@@ -2,11 +2,11 @@ const Command = require("./command_cls.js")
 
 class Cosa extends Command
 {
-  execution(msg)
+  async execution(msg)
   {
     var item = this.get_item();
     var message = `${this.get_mention(msg)}, obtuviste ${item.nombre}${item.emote} de rareza ${item.rareza} \n`;
-    var green_apples = this.get_bot_state().modify_green_apples(this.get_mention(msg), item.rareza);
+    var green_apples = await this.get_bot_state().modify_green_apples(this.get_mention(msg), item.rareza);
     message += `Obtuviste ${item.rareza}<:ManzanaVerde:1296171434246410380>, y ahora tienes un total de ${green_apples}`;
 
     msg.reply(message);
