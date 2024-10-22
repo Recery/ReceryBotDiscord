d518841638c7f946f57fee14b28eabdf981da105
Recery <142628253+Recery@users.noreply.github.com>
2024-10-21 21:41:23 -0300

    Update Manzana_Verde.js

--- Scripts/Commands/Manzana_Verde.js
+++ Scripts/Commands/Manzana_Verde.js
@@ -7,7 +7,7 @@ class ManzanaVerde extends Command
     {
         let green_apples = await this.get_bot_state().get_green_apples(this.get_mention(msg));
 
-        msg.reply(`${this.get_mention(msg)}, ahora tenes ${green_apples}<:ManzanaVerde:1296171434246410380>`)
+        msg.reply(`${this.get_mention(msg)}, tu balance total es de ${green_apples}<:ManzanaVerde:1296171434246410380>`)
     }
 }
 
