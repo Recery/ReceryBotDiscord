64efab440e284534d89c100ca09e45516d482c8e
Recery <142628253+Recery@users.noreply.github.com>
2024-10-21 21:31:56 -0300

    adding green apples to cosa

--- Scripts/bot_state.js
+++ Scripts/bot_state.js
@@ -20,12 +20,62 @@ async function modify_green_apples(mention, amount)
     });
 
     let green_apples = 0;
+    let id = 0;
+    let add_row = true;
 
+    const [rows] = await conex.execute('SELECT * FROM users_green_apples');
+    for (const row of rows)
+    {
+        if (mention === row.mention)
+        {
+            id = row.id;
+            await conex.execute(`UPDATE users_green_apples SET green_apples = green_apples + ${amount} WHERE id = ?`, [id]);
+            
+            const [updated_row] = await conex.execute('SELECT green_apples FROM users_green_apples WHERE id = ?', [id]);
+            green_apples = updated_row[0].green_apples;
 
+            add_row = false;
+            break;
+        }
+    }
+
+    if (add_row)
+    {
+        await conex.execute('INSERT INTO users_green_apples (mention, green_apples) VALUES (?, ?)', [mention, green_apples + amount]);
+    }
+
+    await conex.end();
+
+    return green_apples;
+}
+
+async function get_green_apples(mention)
+{
+    const conex = await mysql.createConnection({
+        uri: process.env.db,
+        ssl: {rejectUnauthorized: false}
+    });
+
+    let green_apples = 0;
+    let id = 0;
+
+    const [rows] = await conex.execute('SELECT * FROM users_green_apples');
+    for (const row of fows)
+    {
+        if (mention === row.mention)
+        {
+            green_apples = row.green_apples;
+        }
+    }
+
+    conex.end();
+
+    return green_apples;
 }
 
 module.exports = { 
     set_asleep,
     get_asleep,
-    modify_green_apples
+    modify_green_apples,
+    get_green_apples
 };
