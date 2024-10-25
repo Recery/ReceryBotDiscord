81493b89a03493e2505a3795ce541c79d0036fec
Recery <142628253+Recery@users.noreply.github.com>
2024-10-25 16:22:02 -0300

    ...

--- Scripts/Commands/Invadir_Peru.js
+++ Scripts/Commands/Invadir_Peru.js
@@ -2,11 +2,14 @@ const Command = require("./command_cls.js")
 
 class Invadir_Peru extends Command
 {
-    execution(msg)
+    async execution(msg)
     {
+        let green_apples = Math.floor(Math.random() * 5) + 1;
+        await this.get_bot_state().modify_green_apples(this.get_mention(msg), green_apples);
+
         msg.reply(
         {
-            content: "Comenzó la operación invadir Perú, preparen sus armas",
+            content: `Invadiste Perú y saqueaste ${green_apples}<:ManzanaVerde:1296171434246410380>`,
             files: [this.get_gifs_directory() + "/Armas.gif"]
         });
     }
