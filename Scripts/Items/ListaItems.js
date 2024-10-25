17dcf6c4303d7a063af494e477837e6364c466dc
Recery <142628253+Recery@users.noreply.github.com>
2024-10-25 16:07:51 -0300

    Update ListaItems.js

--- Scripts/Items/ListaItems.js
+++ Scripts/Items/ListaItems.js
@@ -2,7 +2,8 @@ const Item = require(process.cwd() + "/Scripts/Items/Item");
 
 let items = [
     get_item(1, "Huevo de slime", "<:Huevo1:1298713803214688375>", "Un huevo para incubar un slime.", 5, 3),
-    get_item(2, "Mierda de Slime", ":poop:", "Un sorete de Slime.", 1, 0)
+    get_item(2, "Mierda de Slime", ":poop:", "Un sorete de Slime.", 1, 0),
+    get_item(3, "Tacita de Kofi", "<:TacitaKofi:1299449161086926909>", "La tacita de Kofi", 10, 7)
 ]
 
 function get_item(id, name, emote, description, buy = -1, sell = -1)
