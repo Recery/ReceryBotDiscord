f5fbca2f9ed19cd2bc4f8c2630560b017aa6861a
Recery <142628253+Recery@users.noreply.github.com>
2024-10-29 23:02:43 -0300

    Create Dominante.js

---
+++ Scripts/Commands/Dominante.js
@@ -0,0 +1,14 @@
+const Command = require("./command_cls.js")
+
+class Dominante extends Command
+{
+    execution(msg)
+    {
+        if (this.get_content(msg) !== "")
+            msg.reply(`${this.get_content(msg)} es un ${Math.floor(Math.random() * 100) + 1}% dominante`);
+        else
+            msg.reply(`${this.get_mention(msg)} es un ${Math.floor(Math.random() * 100) + 1}% dominante`)
+    }
+}
+
+module.exports = new Dominante("!dominante");
