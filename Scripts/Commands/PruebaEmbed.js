52a83709bae13e77351830572a363ce6302a7cad
Recery <142628253+Recery@users.noreply.github.com>
2024-11-06 17:10:35 -0300

    Update PruebaEmbed.js

--- Scripts/Commands/PruebaEmbed.js
+++ Scripts/Commands/PruebaEmbed.js
@@ -7,7 +7,8 @@ class PruebaEmbed extends Command
     {
         const embed = new EmbedBuilder()
             .setColor("#00ff00")
-            .setTitle("Embed de Recery Bot");
+            .setTitle("Embed de Recery Bot")
+            .setURL("https://discord.com/oauth2/authorize?client_id=1295826584153882744&permissions=1689934340028480&integration_type=0&scope=bot+applications.commands")
 
         msg.reply({embeds: [embed]});
     }
