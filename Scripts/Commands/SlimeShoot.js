a4a88e1e14ca356a27295e040e532e09ee917ab2
Recery <142628253+Recery@users.noreply.github.com>
2024-11-06 18:47:27 -0300

    added embed slime shoot command

--- Scripts/Commands/SlimeShoot.js
+++ Scripts/Commands/SlimeShoot.js
@@ -1,10 +1,21 @@
+const { EmbedBuilder } = require("discord.js");
 const Command = require("./command_cls.js")
 
 class SlimeShoot extends Command
 {
     execution(msg)
     {
-        msg.reply("> Mi creador, además de crearme a mí, también creó un juego muy interesante... \n > Lo podés descargar gratis acá: https://recery.itch.io/slime-shoot")
+        const embed = new EmbedBuilder()
+            .setColor("#00db3a")
+            .setTitle("¡Click aquí para instalar Slime Shoot!")
+            .setURL("https://recery.itch.io/slime-shoot")
+            .setDescription("Mi creador, además de crearme a mí, también creó un juego muy interesante... \n Lo podés descargar gratis haciendo click en el link de arriba.")
+            .setImage("attachment://SlimeShoot.png");
+
+        msg.reply({
+            embeds: [embed],
+            files: [{ attachment: this.get_image_directory() + "/SlimeShoot.png", name: "SlimeShoot.png" }]
+        });
     }
 }
 
