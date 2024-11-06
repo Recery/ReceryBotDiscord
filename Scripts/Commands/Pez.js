fa303ee1d40e991e1d420db9edc993440b15faee
Recery <142628253+Recery@users.noreply.github.com>
2024-11-06 17:43:33 -0300

    Setting an embed for !pez command

--- Scripts/Commands/Pez.js
+++ Scripts/Commands/Pez.js
@@ -1,15 +1,27 @@
+const { EmbedBuilder } = require("discord.js");
 const Command = require("./command_cls.js")
 
 class Pez extends Command
 {
+    peces = [
+        {nombre: "Goldfish", image: this.get_image_directory() + "/Peces/Goldfish.jpg"},
+        {nombre: "Betta", image: this.get_image_directory() + "/Peces/Betta.jpg"},
+        {nombre: "Escalar", image: this.get_image_directory() + "/Peces/Escalar.jpg"},
+        {nombre: "Cebrita", image: this.get_image_directory() + "/Peces/Cebrita.jpg"},
+        {nombre: "Guppy", image: this.get_image_directory() + "/Peces/Guppy.jpg"},
+        {nombre: "Corydora Paleatus", image: this.get_image_directory() + "/Peces/CoryPaleatus.jpg"}
+    ];
+
     execution(msg)
     {
-        msg.reply(
-            {
-                content: "Un pez",
-                files: [this.get_image_directory() + "/goldfish.jpg"]
-            }
-        );
+        let pez = this.peces[Math.floor(Math.random() * this.peces.length)];
+
+        const embed = new EmbedBuilder()
+            .setColor("#65a7fc")
+            .setTitle(pez.nombre)
+            .setImage(pez.image);
+
+        msg.reply({ embeds: [embed] });
     }
 }
 
