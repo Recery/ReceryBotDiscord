17b9bf4f822323fe4797771fc5a3aa8fb59b0511
Recery <142628253+Recery@users.noreply.github.com>
2024-11-06 18:35:48 -0300

    added embed to invite command

--- Scripts/Commands/ReceryInvite.js
+++ Scripts/Commands/ReceryInvite.js
@@ -1,10 +1,21 @@
+const { EmbedBuilder, Attachment } = require("discord.js");
 const Command = require("./command_cls.js")
 
 class ReceryInvite extends Command
 {
     execution(msg)
     {
-        msg.reply("> ¿Querés invitarme a tu server? \n > Podés hacerlo a través de este link: https://discord.com/oauth2/authorize?client_id=1295826584153882744&permissions=1689934340028480&integration_type=0&scope=bot+applications.commands")
+        const embed = new EmbedBuilder()
+            .setColor("#aa54eb")
+            .setTitle("¡Click aquí para invitarme a tu servidor!")
+            .setURL("https://discord.com/oauth2/authorize?client_id=1295826584153882744&permissions=1689934340028480&integration_type=0&scope=bot+applications.commands")
+            .setDescription("¿Querés invitarme a tu servidor? ¡Hace click en el link de arriba!")
+            .setImage("attachment://ReceryBot.png");
+
+        msg.reply({
+            embeds: [embed],
+            files: [{ attachment: this.get_image_directory() + "/ReceryBot.png", name: "ReceryBot.png" }]
+        });
     }
 }
 
