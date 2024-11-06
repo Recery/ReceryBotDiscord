c1391218aa1c33f0292d1069c53f6fd951d6fae1
Recery <142628253+Recery@users.noreply.github.com>
2024-11-06 17:46:35 -0300

    Update Pez.js

--- Scripts/Commands/Pez.js
+++ Scripts/Commands/Pez.js
@@ -7,7 +7,7 @@ class Pez extends Command
         {nombre: "Goldfish", image: this.get_image_directory() + "/Peces/Goldfish.jpg"},
         {nombre: "Betta", image: this.get_image_directory() + "/Peces/Betta.jpg"},
         {nombre: "Escalar", image: this.get_image_directory() + "/Peces/Escalar.jpg"},
-        {nombre: "Cebrita", image: this.get_image_directory() + "/Peces/Cebrita.jpg"},
+        //{nombre: "Cebrita", image: this.get_image_directory() + "/Peces/Cebrita.jpg"},
         {nombre: "Guppy", image: this.get_image_directory() + "/Peces/Guppy.jpg"},
         {nombre: "Corydora Paleatus", image: this.get_image_directory() + "/Peces/CoryPaleatus.jpg"}
     ];
