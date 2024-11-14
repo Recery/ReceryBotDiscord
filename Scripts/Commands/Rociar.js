4f781e513a0b335aeaf18fcf57f835e18cb90ea8
Recery <142628253+Recery@users.noreply.github.com>
2024-11-13 23:13:52 -0300

    Update Rociar.js

--- Scripts/Commands/Rociar.js
+++ Scripts/Commands/Rociar.js
@@ -1,13 +1,22 @@
 const Command = require("./command_cls.js")
+const { EmbedBuilder } = require("discord.js");
 
 class Rociar extends Command
 {
     async execution(msg)
     {
+        let reply = "";
+
         if (this.get_content(msg) !== "")
-            msg.reply(`**${this.get_mention(msg)}** roció a **${this.get_content(msg)}**... ¿Con qué sustancia? No lo sabemos.`);
+            reply = `**${this.get_mention(msg)}** roció a **${this.get_content(msg)}**... ¿Con qué sustancia? No lo sabemos.`
         else
-            msg.reply(`**${this.get_mention(msg)}** acabó de autorociarse... ¿Con agua? ¿Quizás tenía calor?`)
+            reply = `**${this.get_mention(msg)}** acabó de autorociarse... ¿Con agua? ¿Quizás tenía calor?`
+
+        let embed = new EmbedBuilder()
+            .setColor("#65a7fc")
+            .setDescription(reply);
+
+        msg.reply({embeds: [embed]})
     }
 }
 
