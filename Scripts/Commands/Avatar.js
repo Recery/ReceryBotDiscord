bc5e327da26b6623f9adfb955830afd441be6e7d
Recery <142628253+Recery@users.noreply.github.com>
2024-11-27 14:59:41 -0300

    Update Avatar.js

--- Scripts/Commands/Avatar.js
+++ Scripts/Commands/Avatar.js
@@ -3,19 +3,19 @@ const Canvas = require("canvas")
 
 class Avatar extends Command
 {
-    execution(msg)
+    async execution(msg)
     {
         const canvas = Canvas.createCanvas(800,240);
         const ctx = canvas.getContext("2d");
 
-        const img = Canvas.loadImage(msg.author.avatarURL());
+        const img = await Canvas.loadImage(msg.author.avatarURL());
         ctx.drawImage(img, 50, 50, 160, 160);
 
         msg.reply(
         {
             content: `${this.get_mention(msg)}, tu foto de perfil: `,
             files: [{
-                attachment: canvas.toDataURL(), 
+                attachment: canvas.toBuffer(), 
                 name: "avatar.png"
             }]
         });
