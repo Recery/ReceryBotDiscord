5b7bcbf3df6afc94b7737fdcd790e3031eb9693b
Recery <142628253+Recery@users.noreply.github.com>
2024-11-27 15:11:25 -0300

    Update Avatar.js

--- Scripts/Commands/Avatar.js
+++ Scripts/Commands/Avatar.js
@@ -9,7 +9,7 @@ class Avatar extends Command
         const canvas = Canvas.createCanvas(800,240);
         const ctx = canvas.getContext("2d");
 
-        const img = await Canvas.loadImage(msg.author.displayAvatarURL());
+        const img = await Canvas.loadImage(msg.author.displayAvatarURL({size: 1024, dynamic: true}));
         ctx.drawImage(img, 50, 50, 160, 160);
 
         const attachment = new MessageAttachment(canvas.toBuffer(), "avatar.png")
