eb29f5ccabe5adec39b3cd3ba2aa8d724bc0f409
Recery <142628253+Recery@users.noreply.github.com>
2024-11-27 14:41:20 -0300

    added avatar command

---
+++ Scripts/Commands/Avatar.js
@@ -0,0 +1,15 @@
+const Command = require("./command_cls.js")
+
+class Avatar extends Command
+{
+    execution(msg)
+    {
+        msg.reply(
+        {
+            content: `${this.get_mention(msg)}, tu foto de perfil: `,
+            files: [{attachment: msg.author.avatarUrl, name: "avatar.png"}]
+        });
+    }
+}
+
+module.exports = new Avatar("!avatar", "diversion");
