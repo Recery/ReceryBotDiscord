9ae0164d17e753166ddb53796efdae95e9d61569
Recery <142628253+Recery@users.noreply.github.com>
2024-11-27 17:09:44 -0300

    Update Avatar.js

--- Scripts/Commands/Avatar.js
+++ Scripts/Commands/Avatar.js
@@ -17,11 +17,11 @@ class Bienvenida extends Command
         );
         ctx.drawImage(img, 40, 40, 160, 160);
 
-        ctx.font = "50px Impact"
+        ctx.font = "50px Arial"
         ctx.fillStyle = "#ffffff"
         ctx.fillText("¡Bienvenido al server!", 260, 90)
 
-        ctx.font = "60px Impact"
+        ctx.font = "60px Arial"
         ctx.fillStyle = "#ff7700"
         ctx.fillText(`${msg.author.username}`, 300, 170)
 
