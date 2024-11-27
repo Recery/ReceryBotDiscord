712cff1676a0441ec6fe1fc3a46f2cf287014e26
Recery <142628253+Recery@users.noreply.github.com>
2024-11-27 14:55:53 -0300

    added canvas

--- Scripts/Commands/Avatar.js
+++ Scripts/Commands/Avatar.js
@@ -1,14 +1,21 @@
 const Command = require("./command_cls.js")
+const Canvas = require("canvas")
 
 class Avatar extends Command
 {
     execution(msg)
     {
+        const canvas = Canvas.createCanvas(800,240);
+        const ctx = canvas.getContext("2d");
+
+        const img = Canvas.loadImage(msg.author.avatarURL());
+        ctx.drawImage(img, 50, 50, 160, 160);
+
         msg.reply(
         {
             content: `${this.get_mention(msg)}, tu foto de perfil: `,
             files: [{
-                attachment: msg.author.avatarURL(), 
+                attachment: canvas.toDataURL(), 
                 name: "avatar.png"
             }]
         });
