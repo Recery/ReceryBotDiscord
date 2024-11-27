53dc5222a3b7110f91804b7f6bc1f668780cb1ba
Recery <142628253+Recery@users.noreply.github.com>
2024-11-27 17:17:32 -0300

    Update Avatar.js

--- Scripts/Commands/Avatar.js
+++ Scripts/Commands/Avatar.js
@@ -17,11 +17,11 @@ class Bienvenida extends Command
         );
         ctx.drawImage(img, 40, 40, 160, 160);
 
-        ctx.font = "50px Trebuchet MS"
+        ctx.font = 'bold 50px "Trebuchet MS"'
         ctx.fillStyle = "#ffffff"
         ctx.fillText("¡Bienvenido al server!", 260, 90)
 
-        ctx.font = "60px Trebuchet MS Bold"
+        ctx.font = 'bold 60px "Trebuchet MS"'
         ctx.fillStyle = "#ff7700"
         ctx.fillText(`${msg.author.username}`, 300, 170)
 
