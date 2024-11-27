f576b00c1f12716c8c932f0c60c1bcd02c1be835
Recery <142628253+Recery@users.noreply.github.com>
2024-11-27 16:29:48 -0300

    Update Avatar.js

--- Scripts/Commands/Avatar.js
+++ Scripts/Commands/Avatar.js
@@ -9,13 +9,14 @@ class Avatar extends Command
         const canvas = Canvas.createCanvas(800,240);
         const ctx = canvas.getContext("2d");
 
-        ctx.font = "20px impact"
-        ctx.fillText = (`${msg.author.username}`, 400, 120)
-
         const img = await Canvas.loadImage(
             msg.author.displayAvatarURL({extension: "png", size: 128})
         );
         ctx.drawImage(img, 50, 50, 160, 160);
+
+        ctx.font = "20px impact"
+        ctx.fillStyle = "#f0f0f0"
+        ctx.fillText = (`${msg.author.username}`, 400, 120)
 
         const attachment = new AttachmentBuilder(canvas.toBuffer(), "avatar.png")
 
