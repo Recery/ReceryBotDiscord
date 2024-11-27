931b51672996afbc838d8b7c8cac11d006297841
Recery <142628253+Recery@users.noreply.github.com>
2024-11-27 19:43:41 -0300

    Update prueba.js

--- Scripts/Commands/prueba.js
+++ Scripts/Commands/prueba.js
@@ -10,26 +10,21 @@ class prueba extends Command
         const canvas = Canvas.createCanvas(800,240);
         const ctx = canvas.getContext("2d");
     
-        const background = await Canvas.loadImage(setting.imgsrc)
+        const background = await Canvas.loadImage("https://i.imgur.com/R5z3Xn9.jpeg")
         ctx.drawImage(background, 0, 0, canvas.width, canvas.height)
-    
-        const img = await Canvas.loadImage(
-            member.displayAvatarURL({extension: "png", size: 1024})
-        );
-        ctx.drawImage(img, 40, 40, 160, 160);
     
         ctx.font = '50px "Arial"'
         ctx.fillStyle = "#ffffff"
-        ctx.fillText(setting.message, 260, 90)
+        ctx.fillText("setting.message", 260, 90)
     
         ctx.font = '60px "Arial"'
         ctx.fillStyle = "#ff7700"
-        ctx.fillText(`${member.user.username}`, 300, 170)
+        ctx.fillText(`qwddwqdwq`, 300, 170)
     
         const attachment = new AttachmentBuilder(canvas.toBuffer(), {name: "bienvenidarecery.png"})
     
         const embed = new EmbedBuilder()
-            .setTitle(`¡Bienvenid@ ${member.user.username} al Estanque de Recery!`)
+            .setTitle(`¡Bienvenid@ qwdfdfwq al Estanque de Recery!`)
             .setDescription(`<:ReceryBot:1311098123191455784>¡Ha nacido un alevin!<:ReceryBot:1311098123191455784>
                 Bienvenido al estanque, pequeño pez.
                 Aquí puedes hacer muchas cosas acuáticas como:
