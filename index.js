5573f3b0b5cabf3a53770a89f8c96a64013afab1
Recery <142628253+Recery@users.noreply.github.com>
2024-12-04 22:42:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -30,8 +30,11 @@ client.on(Events.MessageCreate, (msg) => {
 	const content = msg.content.slice(msg.content.indexOf(prefix) + 1)
 	console.log(content);
 
-	if (msg.author.id !== "106915273182285834")
+	if (msg.author.id !== "1069155273182285834")
+	{
 		msg.reply("Perdón pero estoy en mantenimiento... Mi papa está arreglando algunas cosas.");
+		return;
+	}
 
 	if (content === "receryhelp")
   	{
