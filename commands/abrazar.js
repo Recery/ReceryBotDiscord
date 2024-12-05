bdef367824c1b90495813c6a33adfb6d23aa875b
Recery <142628253+Recery@users.noreply.github.com>
2024-12-05 02:11:06 -0300

    Update abrazar.js

--- commands/abrazar.js
+++ commands/abrazar.js
@@ -2,6 +2,13 @@ module.exports = {
     name: "abrazar",
     execute(msg, args)
     {
+        if (!args.length > 0)
+        {
+            msg.reply(`<@${msg.author.id}> se abrazó a si mismo...`;
+                return;
+            )
+        }
+
         const hug_target = args.join(" ");
 
         msg.reply(`<@${msg.author.id}> abrazó a ${hug_target}!`)
