c27ee2a6df49a76741d80995ab9b7d4f567c1959
Recery <142628253+Recery@users.noreply.github.com>
2024-12-15 18:23:20 -0300

    Create setLang.js

---
+++ commands/setLang.js
@@ -0,0 +1,26 @@
+const Langs = require("./../langsLoader.js");
+
+module.exports = {
+    name: "setlang",
+    category: "Administración",
+    execute(client, msg, args)
+    {
+        if (!args.length > 0) 
+        {
+            msg.reply("Debes ingresar un idioma válido.");
+            return;
+        }
+        else if (args[0].length !== "es" && args[0].length !== "en")
+        {
+            msg.reply("Debes ingresar un idioma válido.");
+            return;
+        }
+
+        const serverid = msg.guildId;
+        const new_lang = args[0];
+
+        Langs.set_lang(client, serverid, new_lang);
+
+        msg.reply("El idioma fue cambiado a `" + new_lang + "` con éxito.");
+    }
+}
