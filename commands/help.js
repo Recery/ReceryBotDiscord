42bc222973b99b8f26a5969c99bec23073947568
Recery <142628253+Recery@users.noreply.github.com>
2024-12-15 21:29:25 -0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -12,6 +12,17 @@ module.exports = {
             .setTitle(messages[lang].title)
             .addFields(
             )
+        
+        const selection = new Discord.StringSelectMenuBuilder()
+            .setCustomId("helpCategoriesSel")
+            .setPlaceholder(messages[lang].selection_placeholder)
+            .addOptions(
+                new Discord.StringSelectMenuOptionBuilder()
+                    .setLabel(messages[lang].action_selection_label)
+                    .setDescription(messages[lang].action_selection_label)
+                    .setValue("help_action_option")
+            )
+
 
         msg.reply({embeds: [embed]});
     }
@@ -19,9 +30,15 @@ module.exports = {
 
 const messages = {
     es: {
-        title: "Comandos de Recery Bot"
+        title: "Comandos de Recery Bot",
+        selection_placeholder: "Elige una categoría",
+        action_selection_label: "Acción",
+        action_selection_desc: "Comandos para realizar acciones hacia otros miembros o hacia ti mismo."
     },
     en: {
-        title: "Recery Bot commands"
+        title: "Recery Bot commands",
+        selection_placeholder: "Select a category",
+        action_selection_label: "Action",
+        action_selection_desc: "Commands to perform actions on other members or yourself."
     }
 }
