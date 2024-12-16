4635b62617913cc8a2de12e219f607b3beb6791c
Recery <142628253+Recery@users.noreply.github.com>
2024-12-16 17:06:44 -0300

    adding a tts test

---
+++ commands/tts.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const fetch = require("node-fetch")
+const fs = require("fs");
+
+module.exports = {
+    name: "tts",
+    async execute (client, msg, args) 
+    {
+        const lang = client.langs.get(msg.guildId) || "es";
+
+        const ttsURL = `https://api.freetts.com/generate?lang=es-ES&voice=es-MX-Standard-A&text=${encodeURIComponent(messages[lang])}`;
+        const response = await fetch(ttsURL);
+        const buffer = await response.buffer();
+
+        const file_path = "tts_output.mp3";
+        fs.writeFileSync(file_path, buffer);
+
+        const attachment = new Discord.AttachmentBuilder(file_path, { name: "tts_output.mp3"});
+
+
+        await msg.reply({files: [attachment]});
+
+        fs.unlinkSync(file_path);
+    },
+};
+
+const messages = {
+    es: "Saludos usuario pedazo de pelotudo",
+    en: "Hi user piece of dumbass"
+}
