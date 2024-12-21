5b57cd9db89e70de5df47e327838ec2300ff00d1
Recery <142628253+Recery@users.noreply.github.com>
2024-12-20 22:48:15 -0300

    Create Regalito.js

---
+++ Scripts/Commands/Regalito.js
@@ -0,0 +1,17 @@
+const Command = require("./command_cls.js")
+
+class Regalito extends Command
+{
+    execution(msg)
+    {
+        if (msg.author.id !== "1296846133489963049") 
+        {
+            msg.reply("Usuario no autorizado.");
+            return;
+        }
+
+        msg.reply(`Saludos ${this.get_mention(msg)} pedazo de pelotudo`)
+    }
+}
+
+module.exports = new Regalito("!regalito", "diversion");
