aa695b5f801c7d451fd6f770de65c3ad3dc7ed39
Recery <142628253+Recery@users.noreply.github.com>
2024-12-26 22:30:23 -0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -18,10 +18,18 @@ module.exports = {
             .setPlaceholder(messages[lang].selection_placeholder)
             .addOptions(
                 new Discord.StringSelectMenuOptionBuilder()
+                    .setLabel(messages[lang].administration_selection_label)
+                    .setDescription(messages[lang].administration_selection_desc)
+                    .setValue("administration"),
+                new Discord.StringSelectMenuOptionBuilder()
                     .setLabel(messages[lang].action_selection_label)
                     .setDescription(messages[lang].action_selection_desc)
-                    .setValue("action")
-            )
+                    .setValue("action"),
+                new Discord.StringSelectMenuOptionBuilder()
+                    .setLabel(messages[lang].fun_selection_label)
+                    .setDescription(messages[lang].fun_selection_desc)
+                    .setValue("fun")
+            );
 
         const row1 = new Discord.ActionRowBuilder()
             .addComponents(selection);
@@ -34,13 +42,27 @@ const messages = {
     es: {
         title: "Comandos de Recery Bot",
         selection_placeholder: "Elige una categoría",
+
+        administration_selection_label: "Administración",
+        administration_selection_desc: "Comandos de administración del servidor y configurarme.",
+
         action_selection_label: "Acción",
-        action_selection_desc: "Comandos para realizar acciones hacia otros miembros o hacia ti mismo."
+        action_selection_desc: "Comandos para realizar acciones hacia otros miembros o hacia ti mismo.",
+
+        fun_selection_label: "Diversión",
+        fun_selection_desc: "Comandos para divertirte."
     },
     en: {
         title: "Recery Bot commands",
         selection_placeholder: "Choose a category",
+
+        administration_selection_label: "Administration",
+        administration_selection_desc: "Commands to manage the server and configure myself.",
+
         action_selection_label: "Action",
-        action_selection_desc: "Commands to perform actions on other members or yourself."
+        action_selection_desc: "Commands to perform actions on other members or yourself.",
+
+        fun_selection_label: "Fun",
+        fun_selection_desc: "Commands to have fun."
     }
 }
