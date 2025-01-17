cb7910dc59f9c9e43ce981117f559a85a6d5e422
Recery <142628253+Recery@users.noreply.github.com>
2025-01-17 18:27:41 -0300

    adding more saludar commands, just for testing

---
+++ commands/saludar copy 2.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: "saludar3",
+    category: "action",
+    execute (client, msg, args) 
+    {
+        const lang = client.langs.get(msg.guildId) || "es";
+        msg.reply(messages[lang].replace("{{id}}", msg.author.id));
+    },
+};
+
+const messages = {
+    es: "Saludos <@{{id}}> pedazo de pelotudo",
+    en: "Hi <@{{id}}> piece of dumbass"
+}
