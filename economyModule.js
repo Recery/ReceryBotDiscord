1d6cd57bf0e6dabde26a4fbfe465412c4708dfdb
Recery <142628253+Recery@users.noreply.github.com>
2025-01-19 01:18:50 -0300

    didnt work, trying something else

--- economyModule.js
+++ economyModule.js
@@ -3,17 +3,34 @@ const DB = require("better-sqlite3");
 const db =  new DB(process.env.ECONOMY_DB_PATH);
 
 // Se manda un valor nuevo para las manzanas y lo reemplaza completamente al valor anterior
-async function modifyApples(userID, apples) {
-    await db.prepare("INSERT OR REPLACE INTO greenApples (userId, apples) VALUES (?, ?)").run(userID, apples);
+function modifyApples(userID, apples) {
+    while (true) {
+        try {
+            db.prepare("INSERT OR REPLACE INTO greenApples (userId, apples) VALUES (?, ?)").run(userID, apples);
+            break;
+        }
+        catch (err) {
+            if (err.code !== 'SQLITE_BUSY') throw err;
+        }
+    }
 }
 
-async function getApples(userID) {
-    const row = await db.prepare("SELECT apples FROM greenApples WHERE userId = ?").get(userID);
+function getApples(userID) {
+    while (true) {
+        try {
+            const row = db.prepare("SELECT apples FROM greenApples WHERE userId = ?").get(userID);
 
-    if (row)
-        return row.apples;
-    else 
-        return 0;
+            if (row)
+                return row.apples;
+            else 
+                return 0;
+            
+            break;
+        }
+        catch (err) {
+            if (err.code !== 'SQLITE_BUSY') throw err;
+        }
+    }
 }
 
 module.exports = {
