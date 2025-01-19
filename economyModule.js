c658f7c021fa48826e044069257b46f9244da339
Recery <142628253+Recery@users.noreply.github.com>
2025-01-19 01:26:48 -0300

    Update economyModule.js

--- economyModule.js
+++ economyModule.js
@@ -1,19 +1,15 @@
 const DB = require("better-sqlite3");
 
+const db =  new DB(process.env.ECONOMY_DB_PATH);
+db.pragma("journal_mode = WAL");
+
 // Se manda un valor nuevo para las manzanas y lo reemplaza completamente al valor anterior
 function modifyApples(userID, apples) {
-    const db =  new DB(process.env.ECONOMY_DB_PATH);
-
     db.prepare("INSERT OR REPLACE INTO greenApples (userId, apples) VALUES (?, ?)").run(userID, apples);
-
-    db.close();
 }
 
 function getApples(userID) {
-    const db =  new DB(process.env.ECONOMY_DB_PATH);
-    
     const row = db.prepare("SELECT apples FROM greenApples WHERE userId = ?").get(userID);
-    db.close();
 
     if (row)
         return row.apples;
