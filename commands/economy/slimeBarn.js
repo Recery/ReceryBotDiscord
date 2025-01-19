12f0bac1dfe74c3932f32dcfd05836afeff18dce
Recery <142628253+Recery@users.noreply.github.com>
2025-01-19 20:10:52 -0300

    Update slimeBarn.js

--- commands/economy/slimeBarn.js
+++ commands/economy/slimeBarn.js
@@ -5,8 +5,17 @@ module.exports = {
     alias: ["sb", "barn", "granero", "graneroslime"],
     category: "economy",
     execute(client, msg, args) {
+        const lang = client.langs.get(msg.guildId) || "es";
         const userID = msg.author.id;
 
-        msg.reply(`<@${userID}>, en total tienes ${eco.getBarnSlimesAmount(userID)} slimes en tu granero.`);
+        const slimes = eco.getBarnSlimes(userID);
+
+        let content = "\n";
+        if (!slimes.length > 0) content += "¡Ninguno!"
+
+        for (const slime of eco.getBarnSlimes(userID))
+            content += slime.slime.displayName[lang] + " x" + slime.amount.toString();
+
+        msg.reply(`<@${userID}>, en tienes estos slimes en tu granero: ${content}`);
     }
 }
