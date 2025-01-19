7b5ec088316390b05dc9adbc1b159ab48115c99f
Recery <142628253+Recery@users.noreply.github.com>
2025-01-19 00:30:23 -0300

    Update hatchSlimes.js

--- commands/economy/hatchSlimes.js
+++ commands/economy/hatchSlimes.js
@@ -15,14 +15,37 @@ module.exports = {
         if (userApples < applesToSpend) {
             msg.reply(messages[lang].needMoreApples.replace("{{apples}}", applesToSpend.toString()));
         }
+        else {
+            let hatchedSlimes = "**" + messages[lang].slimeObtention + "**\n";
+            for (let i = 0; i < slimesToHatch; i++) {
+                hatchedSlimes += " - " + slimes[Math.floor(Math.random() * slimes.length)];
+                if (i < slimesToHatch - 1) hatchedSlimes += "\n";
+            }
+            msg.reply(hatchedSlimes);
+        }
     }
 }
 
+const slimes = [
+    "green slime",
+    "yellow slime",
+    "white slime",
+    "cosmic slime",
+    "creamy slime",
+    "poo slime",
+    "goldfish slime",
+    "mummified slime",
+    "red slime",
+    "blue slime"
+]
+
 const messages = {
     es: {
-        needMoreApples: "¡No tienes suficientes :green_apple: para eclosionar esa cantidad de slimes!\nNecesitas {{apples}}:green_apple:."
+        needMoreApples: "¡No tienes suficientes :green_apple: para eclosionar esa cantidad de slimes!\nNecesitas {{apples}}:green_apple:.",
+        slimeObtention: "¡Eclosionaste estos slimes!"
     },
     en: {
-        needMoreApples: "You don't have enough :green_apple: to hatch that amount of slimes!\n You need {{apples}}:green_apple:."
+        needMoreApples: "You don't have enough :green_apple: to hatch that amount of slimes!\n You need {{apples}}:green_apple:.",
+        slimeObtention: "You hatched these slimes!"
     }
 }
