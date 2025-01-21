648958684259476fb919ca30ba8c5c6d8cde15ba
Recery <142628253+Recery@users.noreply.github.com>
2025-01-21 20:57:48 -0300

    Update corralTimeleft.js

--- commands/economy/corralTimeleft.js
+++ commands/economy/corralTimeleft.js
@@ -10,13 +10,38 @@ module.exports = {
 
         const timeleft = eco.getCorralResetTimeLeft(userID);
 
-        const minutes = Math.floor(1 + (timeleft / 1000) / 60);
+        // Obtener minutos, en base a los minutos ver si debe expresar el tiempo en horas, minutos o segundos 
+        const minutes = Math.floor((timeleft / 1000) / 60);
 
-        msg.reply(messages[lang].replace("{{minutes}}", minutes.toString()));
+        // Por defecto expresar en minutos
+        let time = minutes;
+        let unit = messages[lang].minutes;
+
+        if (minutes > 60) { // Más de una hora, expresar en horas
+            time = Math.floor(time / 60);
+            unit = messages[lang].hours;
+        }
+        else if (minutes <= 0) { // Menos de un minuto, expresar en segundos
+            time = Math.floor(minutes * 60);
+            unit = messages[lang].seconds;
+        }
+
+
+        msg.reply(messages[lang].replace("{{time}}", time.toString()).replace("{{unit}}", messages[lang][unit]));
     }
 }
 
 const messages = {
-    es: "Tu corral se reiniciará en {{minutes}} minutos.",
-    en: "Your corral will reset in {{minutes}} minutes."
+    es: {
+        seconds: "segundos",
+        minutes: "minutos",
+        hours: "horas",
+        reply: "Tu corral se reiniciará en {{time}} {{unit}}.",
+    },
+    en: {
+        seconds: "seconds",
+        minutes: "minutes",
+        hours: "hours",
+        reply: "Your corral will reset in {{time}} {{unit}}."
+    }
 }
