1d51ab5755c54424728cc0ece80a6498a767cb28
Recery <142628253+Recery@users.noreply.github.com>
2025-01-22 02:18:16 -0300

    Update slimeInfo.js

--- commands/economy/slimeInfo.js
+++ commands/economy/slimeInfo.js
@@ -40,7 +40,7 @@ module.exports = {
             .setColor("#12bcff")
             .setDescription(description)
             .setThumbnail(slimeObj.image)
-            .setFooter({text: msg.author.displayName, iconURL: msg.author.avatarDecorationURL()});
+            .setFooter({text: msg.author.displayName, iconURL: msg.author.avatarURL()});
 
         msg.reply({
             embeds: [embed],
